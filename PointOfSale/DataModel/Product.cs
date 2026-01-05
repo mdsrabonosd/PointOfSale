fca63b87263be7c2344940f430e8386a9843201a
MdSrabon <mdsrabon.cse@gmail.com>
2026-01-05 23:39:49 +0600

    ProductDataMode_l

---
+++ PointOfSale/DataModel/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PointOfSale.DataModel
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SupplyerId { get; set; }
+        public int CatagoryId { get; set; }
+        public double BuyPrice { get; set; }
+        public double SalePrice { get; set; }
+        public IFormFile Image { get; set; }
+        public DateTime ExpireDate { get; set; }
+
+
+
+    }
+}
